8d5a2df0fb9da3c213d91e9deadd8b6f6eae6501
Uemerson Pinheiro Junior <uemersonpinheirojunior@gmail.com>
2024-04-19 17:18:49 -0300

    feat: add generate random password pkg

---
+++ pkg/password/generate.go
@@ -0,0 +1,22 @@
+package password
+
+import (
+	"math/rand"
+	"time"
+)
+
+const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"
+
+func generateRandomString(length int) string {
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	b := make([]byte, length)
+	for i := range b {
+		b[i] = charset[r.Intn(len(charset))]
+	}
+	return string(b)
+}
+
+func Generate(length int) string {
+	randomString := generateRandomString(length)
+	return randomString
+}
